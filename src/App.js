371e30a4988fba7cafa1cbb05412d38a9a0552b1
Gauravtripathii <gauravtripathii7979@gmail.com>
2022-01-08 12:15:31 +0530

    initial commit 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import React, {useState} from 'react';
 import './App.css';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="App">
+      <p>React Todo</p>
+      <TodoForm/>
+      <TodoList/>
     </div>
   );
 }
---
+++ src/components/TodoForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TodoForm() {
+    return(
+        <form>
+            <input type="text"/>
+            <input type="button" value="add" />
+        </form>
+    );
+}
+
+export default TodoForm;
